b4659997701dba066406c1ec20c3a616e5927973
Andrea Torelli <gioran326@protonmail.com>
2023-06-05 17:14:31 +0200

    tolto il paginate

--- app/Http/Controllers/Api/CarController.php
+++ app/Http/Controllers/Api/CarController.php
@@ -4,19 +4,16 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Car;
-use Illuminate\Http\Request;
 
 class CarController extends Controller
 {
 
     public function index()
     {
-        $cars = Car::with('optionals')->paginate(6);
+        $cars = Car::with('optionals')->get();
         return response()->json([
             'success' => true,
             'results' => $cars
-
-
         ]);
 
     }
