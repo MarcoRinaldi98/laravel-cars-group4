d6005df23d9aff689c8901e16a2889924443fb12
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-06-05 15:58:02 +0200

    Implementazione controllers e rotte api

---
+++ app/Http/Controllers/Api/CarController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Car;
+use Illuminate\Http\Request;
+
+class CarController extends Controller
+{
+
+    public function index()
+    {
+        $cars = Car::with('optionals')->paginate(6);
+        return response()->json([
+            'success' => true,
+            'results' => $cars
+
+
+        ]);
+
+    }
+
+    public function show($id)
+    {
+        $car = Car::where('id', $id)->with(['optionals',])->first();
+        if ($car) {
+            return response()->json([
+                'success' => true,
+                'car' => $car
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'error' => 'Auto non trovata!'
+            ]);
+        }
+
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CarController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+Route::get('/cars', [CarController::class, 'index']);
+Route::get('/car/{id}', [CarController::class, 'show']);
